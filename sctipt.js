998abd4c483268e3fe16c748e6ddee89f4f9e596
Furkan Tural <furkantural3328@hotmail.com>
2020-08-31 15:26:29 +0300

    class added

--- sctipt.js
+++ sctipt.js
@@ -1,10 +1,12 @@
 const showModule = document.getElementById('open');
 const closeModule = document.getElementById('close');
 const masage = document.querySelector('.container');
-//const showModule = document.getElementById('open');
+//const body = document.getElementsByTagName('body'), if select these code oter elment cont select problay body crepunding all
+
 
 showModule.addEventListener('click',function(){
-    masage.style.display = 'block';
+    masage.style.display = 'inline-block';
+    //body.style.opacity = '0.5'
 })
 
 closeModule.addEventListener('click',function(){
